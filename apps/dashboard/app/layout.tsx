7291b74506ee0019ae6430736fad6bc55d4a3d75
champ3oy <akotosel6@gmail.com>
2026-04-17 17:26:02 +0000

    feat(dashboard): switch font to Roboto Mono

--- apps/dashboard/app/layout.tsx
+++ apps/dashboard/app/layout.tsx
@@ -1,10 +1,10 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import { Nunito } from "next/font/google";
+import { Roboto_Mono } from "next/font/google";
 import { cn } from "@/lib/utils";
 import { Providers } from "@/lib/providers";
 
-const nunito = Nunito({ subsets: ["latin"], weight: ["500", "600", "700"], variable: "--font-sans" });
+const robotoMono = Roboto_Mono({ subsets: ["latin"], weight: ["400", "500", "600", "700"], variable: "--font-sans" });
 
 export const metadata: Metadata = {
   title: "Pingback",
@@ -17,7 +17,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en" className={cn("dark", "font-sans", nunito.variable)}>
+    <html lang="en" className={cn("dark", "font-sans", robotoMono.variable)}>
       <body className="antialiased font-medium">
         <Providers>{children}</Providers>
       </body>
