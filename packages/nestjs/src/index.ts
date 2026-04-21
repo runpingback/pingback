1aaf401542be473cfb11ea85d136af75d0ee2a38
champ3oy <akotosel6@gmail.com>
2026-04-21 20:22:33 +0000

    feat(nestjs): add public API exports and verify build

--- packages/nestjs/src/index.ts
+++ packages/nestjs/src/index.ts
@@ -1,1 +1,6 @@
-export {};
+export { PingbackModule } from './pingback.module';
+export { Cron, Task } from './decorators';
+export type { PingbackFunctionMetadata } from './decorators';
+export { PINGBACK_OPTIONS, PINGBACK_REGISTRY } from './pingback.controller';
+export type { PingbackModuleOptions } from './pingback.controller';
+export type { Context as PingbackContext } from '@usepingback/core';
